b62edbff5b6cadc0fab6adc619ab1665e6b35a1c
Jakob Ferdinand Wegenschimmel <jwegenschimmel@rzl.at>
2021-09-15 15:42:50 +0200

    Restart ❌🎮🥙

--- src/elm-live-server.sh
+++
@@ -1,6 +0,0 @@
-
-# if elm-live is missing run:
-# npm install --global elm elm-live
-# https://github.com/wking-io/elm-live
-
-elm-live src/Main.elm --start-page=index.html --pushstate -- --output=elm.js --debug
